c43207eaaac17b3ba459ac134321cd5ac9ef6307
Joel Straley <Joelstraley@gmail.com>
2020-12-10 14:50:27 -0500

    added Past, Present, Future color change into for loop AND fixed local storage save

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
-
     var container = $(".container");
     var hour = $(".hour");
-    var saveBtn = $(".saveBtn");
-    var description = $(".description");
     var now = dayjs();
     var rows = 9
     var columns = 3
     var dataHour = 9
-    
+    var present =  parseInt(now.format('H'))
+
     $("#currentDay").text(now.format('dddd, MMMM D'));
+    
+    function setInput() {
+        var time = $(this).siblings(".hour").text();
+        var input = $(this).siblings(".userInput").val();
+        localStorage.setItem(time, input);
+     };
+
+     function save(){
+        $(".hour").each(function(){
+          var currentTime = $(this).text();
+          var storedInput = localStorage.getItem(currentTime);
+          if (storedInput !== null) {
+            $(this).siblings(".userInput").val(storedInput);
+          }
+        })
+      }
      
     for(i=0; i < rows; i++){
       var newRow = $('<div class="row time-block">')
@@ -18,99 +32,27 @@ $(document).ready(function() {
         if(j === 0){
           var newColumn0 = $(`<div id=${dataHour} class="hour col-1">`)
           newColumn0.text(now.hour(dataHour).format('h A'))
-          dataHour++
           newRow.append(newColumn0)
         }else if(j === 1){
-          var newColumn1 = $('<textarea class="description col-10" name="userInput">')
-            newRow.append(newColumn1)
+          var newColumn1 = $('<textarea class="description userInput col-10" name="userInput">')
+          console.log(dataHour, present)
+          if(dataHour < present){
+            newColumn1.addClass("past")
+        }else if(dataHour == present){
+            newColumn1.addClass("present")
+        }else {
+            newColumn1.addClass("future")
+        }
+          newRow.append(newColumn1)
         }else if(j === 2){
           var newColumn2 = $('<button class="saveBtn btn fas fa-save col-1"> input=')
+           newColumn2.on("click", setInput)
            newRow.append(newColumn2)
+           dataHour++
         }
       }
     }
     
-        
-   /*  function pastpresentfuture() {
-      if(parseInt(dataHour) > parseInt(now.format('h'))){
-        description.addClass("past");
-        
-      } else if (parseInt(hour.text()) === parseInt(now.format('h'))){
-        description.addClass("present");
-      } else {
-        description.addClass("future");
-      }     
-    } */
-
-    console.log(parseInt($(hour).attr("id")))
-
-    function pastpresentfuture() {
-        var blockHour = parseInt($(this).attr("id"))
-        var present =  parseInt(now.format('h'))
-        $(".description").each(function(){
-          if(blockHour < present){
-            $(this).addClass("past")
-          }else if(blockHour == present){
-            $(this).removeClass("past")
-            $(this).addClass("present")
-          }else {
-            $(this).removeClass("past")
-            $(this).removeClass("present")
-            $(this).addClass("future")
-          }
-        })
-        }
-
-    pastpresentfuture();  
-    /* */
-    
-    
-    saveBtn.on("click", function() {
-       var time = $(this).siblings(".hour").text();
-       var input = $(this).siblings(".userInput").val();
-       localStorage.setItem(time, input);
-    });
-    
-    function save(){
-      $(".hour").each(function(){
-        var currentTime = $(this).text();
-        var storedInput = localStorage.getItem(currentTime);
-    
-        if (storedInput !== null) {
-          $(this).siblings(".userInput").val(storedInput);
-        }
-      })
-    }
-    
-    
-    pastpresentfuture();
     save();
-    
-    
-    
-    
-    
-    /* 
-    - color-code for Past, Present, Future
-         
-    -Can write into each time-block
-    -Save button save's what's written into local storage
-    -input is still there on page refresh 
-    
-    
-    
-    
-    --------------------------
-    the current day is displayed at the top of the calendar
-    WHEN I scroll down
-    THEN I am presented with time blocks for standard business hours
-    WHEN I view the time blocks for that day
-    THEN each time block is color-coded to indicate whether it is in the past, present, or future
-    WHEN I click into a time block
-    THEN I can enter an event
-    WHEN I click the save button for that time block
-    THEN the text for that event is saved in local storage
-    WHEN I refresh the page
-    THEN the saved events persist */
     
     })
